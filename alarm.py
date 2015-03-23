90095b1447a9f697c655af3dd6f0ab766a9dbb53
ovicin <ovicin@yahoo.com>
2015-03-23 21:46:17 +0000

    updated alarm.py

--- alarm.py
+++ alarm.py
@@ -1,6 +1,6 @@
 import smtplib
 import mraa
-import time
+import time, threading
 
 ZONE_INPUT_PIN = 6
 RELAY_OUTPUT_PIN = 8
@@ -8,17 +8,25 @@ ALARM_LED_PIN = 8
 ALARM_SW_PIN = 7
 
 def zoneInputISR():
+	print("Zone Input ISR")
 	a.UpdateZoneInputState()
 	
 def buttonISR():
-	a.armed = not a.armed
+	print("Button ISR")
+	if a.GetArmedState():
+		a.Disarm()
+	else:
+		a.Arm()
 	
 class Alarm:
 	def __init__(self):
 		
 		self.armed = 0
+		self.arming = 0
 		self.inputState = 0
 		self.relayState = 0
+		self.ledState = 0
+		self.alarmState = 0
 	
 		#configure zone input pin
 		self.zoneInput = mraa.Gpio(ZONE_INPUT_PIN) 
@@ -41,16 +49,53 @@ class Alarm:
 	def GetArmedState(self):
 		return self.armed
 	
+	def SetArmedState(self):
+		print("Armed")
+		self.armed = 1
+		self.arming = 0
+	
 	def Disarm(self):
+		print("Disarmed")
+		self.alarmState = 0
 		self.armed = 0
 	
 	def Arm(self):
 		#start arm timer
-		print("arm alarm")
+		print("Arm Alarm in 10s")
+		self.arming = 1
+		threading.Timer(10, self.SetArmedState).start()
 	
 	def UpdateZoneInputState(self):
 		self.inputState = self.zoneInput.read()
 		
+	def Process(self):
+		print("Armed " + str(self.armed))
+		print("InputState " + str(self.inputState))
+		
+		if self.armed:
+			#Toogle LED
+			self.ledState = not self.ledState
+			if (self.inputState and not self.alarmState):
+				#we are armed and we have an alarm
+				#activate the output relay
+				self.relayState = 1
+				#self.send_email()
+				self.alarmState = 1
+		else:
+			self.relayState = 0
+			if self.arming:
+				#if arming turn the led on
+				self.ledState = 1
+			else:
+				#if not arming turn the led off
+				self.ledState = 0
+				
+		self.led.write(self.ledState)
+		self.relay.write(self.relayState)
+		
+		#Process calls itself each 1s
+		threading.Timer(1, self.Process).start()
+	
 	def send_email():
 		gmail_user = "xxx@gmail.com"
 		gmail_pwd = "xxx"
@@ -75,7 +120,9 @@ class Alarm:
 			print "failed to send mail"
 		
 a = Alarm()
-print("Test")
+a.Process()
+
+
 time.sleep(500)
 	
 
